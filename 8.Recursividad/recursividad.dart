8a2e91e1bc40078295b467d7545c4b2333086b8a
Edwin <edwinmg27@gmail.com>
2026-06-09 17:01:01 -0400

    Agregar implementación de funciones factorial y fibonacci en recursividad.dart

--- 8.Recursividad/recursividad.dart
+++ 8.Recursividad/recursividad.dart
@@ -1,1 +1,33 @@
-void main() {}
+void main() {
+  //Problema tipico: factorial
+  //Factorial es multiplicar un numero base por todos los numero menores que el hasta llegar a 1
+  //El factorial se representa con el simbolo !
+  //Su formula es n! = n(n - 1)!
+
+  //Ejercicio 1 calcular el factorial de 5
+  print(factorial(5));
+
+  //Ejercicio 2 fibonacci fib(5)
+  //cada numero es la suma de los dos anteriores
+  // formula: f(n) = f(n - 1) + f(n - 2)
+  print(fibonacci(5));
+
+  //Ejericcio 3
+}
+
+int factorial(int fact) {
+  if (fact == 1) {
+    return 1;
+  }
+  return fact * factorial(fact - 1);
+}
+
+int fibonacci(int n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
